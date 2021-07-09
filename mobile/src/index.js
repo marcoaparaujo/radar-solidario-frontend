82b92606c522ebc9c7e2f1d30ecfb1edd06fc740
gabruw <gabrielmarques.mg@gmail.com>
2021-07-09 13:21:34 -0300

    feat: Ignore cycle warning

--- mobile/src/index.js
+++ mobile/src/index.js
@@ -1,8 +1,11 @@
 //#region Imports
 
 import { registerRootComponent } from 'expo';
+import { LogBox } from 'react-native';
 import App from './App';
 
 //#endregion
+
+LogBox.ignoreLogs(['Require cycle:']);
 
 registerRootComponent(App);
