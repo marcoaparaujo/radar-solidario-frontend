26c99e833796891800443410792fb9d890c300d0
gabruw <gabrielmarques.mg@gmail.com>
2021-10-21 22:27:08 -0300

    feat: Atualizações dos pacotes e melhorias no eslint

--- mobile/.eslintrc.js
+++ mobile/.eslintrc.js
@@ -1,4 +1,37 @@
 module.exports = {
-  root: true,
-  extends: '@react-native-community',
+    env: {
+        "browser": true,
+        "es2021": true
+    },
+    extends: [
+      "airbnb",
+      "prettier",
+      "prettier/react",
+      "plugin:react/recommended",
+      '@react-native-community'
+    ],
+    globals: {
+      Atomics: "readonly",
+      SharedArrayBuffer: "readonly",
+      __DEV__: 'readonly'
+    },
+    parser: "@babel/eslint-parser",
+    parserOptions: {
+        ecmaFeatures: {
+            jsx: true
+        },
+        ecmaVersion: "latest",
+        sourceType: "module"
+    },
+    plugins: [
+        "react",
+        "prettier"
+    ],
+    rules: {
+      "prettier/prettier": "error",
+      "react/jsx-filename-extension": ["warn", { extensions: [".jsx", ".js"] }],
+      "import/prefer-default-export": "off",
+      "no-param-reassign": "off",
+      "no-console": ["error", { allow: ["tron"] }]
+    }
 };
