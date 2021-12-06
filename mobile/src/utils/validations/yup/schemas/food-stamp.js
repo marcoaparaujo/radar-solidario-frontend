e84139881e0c2df67d0bfca93afc65e364ad171b
gabruw <gabrielmarques.mg@gmail.com>
2021-12-06 09:15:28 -0300

    feat: Correções gerais

--- mobile/src/utils/validations/yup/schemas/food-stamp.js
+++ mobile/src/utils/validations/yup/schemas/food-stamp.js
@@ -2,13 +2,22 @@
 
 import FOOD_STAMP_FIELDS from 'utils/constants/fields/food-stamp';
 import FOOD_STAMP_LABELS from 'utils/constants/labels/food-stamp';
+import emptyStringToUndefined from '../conversors/emptyStringToUndefined';
 import yup from '../yup';
 
 //#endregion
 
 const foodStampSchema = yup.object().shape({
-    [FOOD_STAMP_FIELDS.LENGTH]: yup.number().required().label(FOOD_STAMP_LABELS.LENGTH),
-    [FOOD_STAMP_FIELDS.WEIGHT]: yup.number().required().label(FOOD_STAMP_LABELS.WEIGHT)
+    [FOOD_STAMP_FIELDS.LENGTH]: yup
+        .number()
+        .required()
+        .transform(emptyStringToUndefined)
+        .label(FOOD_STAMP_LABELS.LENGTH),
+    [FOOD_STAMP_FIELDS.WEIGHT]: yup
+        .number()
+        .required()
+        .transform(emptyStringToUndefined)
+        .label(FOOD_STAMP_LABELS.WEIGHT)
 });
 
 export default foodStampSchema;
